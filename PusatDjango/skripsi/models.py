774c14fd86079e39ba92a00187bbbd3ea68570b3
asrulmaaliy26 <asrulmaaliy26@gmail.com>
2025-03-25 23:23:24 +0700

    sudah biki dashboard dll. tinggal uji coba

--- PusatDjango/skripsi/models.py
+++ PusatDjango/skripsi/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class SensorSmartACData(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    ppm = models.FloatField()
+    temp = models.FloatField()
+    humi = models.FloatField()
+    tempout = models.FloatField()
+    humiout = models.FloatField()
+    tempac = models.FloatField()    
+    modeac = models.FloatField()    
+    hasilpred = models.FloatField()    
+
+    def __str__(self):
+        return f"{self.id} | Hasil Prediksi {self.hasilpred}"
